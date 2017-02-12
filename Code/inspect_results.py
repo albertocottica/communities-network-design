2db795fb3fc31ae8be2467f9517e365342a909b0
Benjamin Renoust <benjamin.renoust@gmail.com>
2017-02-12 19:06:20 +0900

    adding a plotting device to inspect extreme cases, updated the global results output

---
+++ Code/inspect_results.py
@@ -0,0 +1,255 @@
+import os
+import powerlaw
+import random as rd
+import datetime
+from pylab import *
+import matplotlib.pyplot as pyplot
+from matplotlib.legend_handler import HandlerLine2D
+import collections
+import math
+
+#csv = "res.csv"
+#csv = "res_wob.csv"
+
+folders = ["../Datasets/FittingData_02_2017/"]
+
+def __drawCCDF(data_serie, table_y_label, plot_title, xmin=None, other=False):
+        #print 'Plotting PDF - ',plot_title
+        global initiated
+
+        if initiated == False:
+
+
+            # cosmetics setting
+            default_ccdf_color = '#e7298a'
+            default_ccdf_linewidth = 2.0
+            default_ccdf_fit_linestyle = '--'
+            default_ccdf_exponential_color = '#7570b3'
+            default_ccdf_powerlaw_color = '#d95f02'
+            default_ccdf_lognormal_color = '#1b9e77'
+
+            # now draws the CCDF against the powerlaw
+            plt.figure("PDF - " + plot_title)
+            plt.title("PDF - " + plot_title)
+            plt.xlabel(table_y_label)
+            plt.grid()
+
+
+        # do different fits (should be optional in the future)
+        if xmin == None:
+            fit = powerlaw.Fit(data_serie, discrete=True)
+        else:
+            fit = powerlaw.Fit(data_serie, discrete=True, xmin=xmin)
+
+        if other == True:
+            default_ccdf_color = default_ccdf_exponential_color
+            default_ccdf_powerlaw_color = default_ccdf_lognormal_color
+
+
+        plot1 = fit.plot_pdf(color=default_ccdf_color, \
+                                linewidth=default_ccdf_linewidth,\
+                                label='empirical', alpha=0.2)
+
+        #plot2 = fit.exponential.plot_pdf (color=default_ccdf_exponential_color, \
+        #                                   linewidth=default_ccdf_linewidth, \
+        #                                   ls=default_ccdf_fit_linestyle, \
+        #                                   label='exponential')
+
+        plot3 = fit.power_law.plot_pdf (color = default_ccdf_powerlaw_color, \
+                                         linewidth=default_ccdf_linewidth, \
+                                         ls=default_ccdf_fit_linestyle, \
+                                         label='powerlaw', alpha=0.2)
+
+        #plot4 = fit.lognormal.plot_pdf (color = default_ccdf_lognormal_color, \
+        #                                 linewidth=2.0, \
+        #                                 ls=default_ccdf_fit_linestyle, \
+        #                                 label='lognormal')
+
+        # places the legend on the bottom left corner
+        #first_legend = plt.legend(handler_map={plot1: HandlerLine2D(numpoints=4)}, loc=3)
+
+        # if we want the distances of fit, here they are
+        # print fit.distribution_compare('power_law', 'lognormal', normalized_ratio=True)
+        # maybe find a way to automatically extract the top n%
+
+        # do not forget the grid!
+        
+        #plt.savefig(self.pictures_root_path+ "CCDF_" + plot_title.replace(' ', '_')+'.'+self.default_file_format,\
+        #            format=self.default_file_format,\
+        #            dpi=self.default_dpi)
+
+
+def __computeDegreeDistribution(degree_list, frequency=False, dict_form=False):
+    counted = collections.Counter(sorted(degree_list))
+    counter = collections.OrderedDict(sorted(counted.items()))
+
+    if dict_form:
+        if frequency:
+            n = float(len(degree_list))
+            for k in counter:
+                couter[k] /= n
+            return counter
+        else:
+            return counter
+
+
+    if frequency:
+        n = float(len(degree_list))
+        return {"indegree":counter.keys(), "count":[x/n for n in counter.values()]}
+    else:
+        return {"indegree":counter.keys(), "count":counter.values()}
+
+    #print(counter)
+    # Counter({1: 4, 2: 4, 3: 2, 5: 2, 4: 1})
+    #print(counter.values())
+    # [4, 4, 2, 1, 2]
+    #print(counter.keys())
+
+
+initiated = False
+
+# this function draws a distribution (basicaly just a plot x/y)
+def __drawDistribution(data, table_x_label, table_y_label, plot_title, color="blue"):
+
+        global initiated
+
+        #plt.figure("Distribution - " + plot_title)
+        #plt.title("Distribution - " + plot_title)
+        #plt.xlabel(table_x_label)
+        #plt.ylabel(table_y_label)
+
+        if initiated == False:
+
+            x_values = [x+1 for x in data[table_x_label]]
+            y_values = data[table_y_label]
+            #print data
+
+            #plt.plot(x_values, y_values)
+            plt.figure("Distribution - " + plot_title)
+            ax = plt.subplot(111)
+
+            plt.title("Distribution - " + plot_title)
+            plt.xlabel(table_x_label)
+            plt.ylabel(table_y_label)
+            #plt.ylim(min(y_values), max(y_values))
+            #plt.xlim(0, 100)
+            plt.grid()
+            initiated = False
+
+        line, = ax.plot(x_values, y_values, color=color, alpha=0.2)#, marker='o')#, color='blue', lw=2)
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+
+
+def read_csv(lines, verbose=False):
+    
+    entries = [l.split(';') for l in lines]
+    #print 'the entries are ', entries[0]
+    #print "the rest is ", entries[1]
+
+    #['alpha_constrained', 'sigma_constrained', 'p_value_constrained', 'alpha', 'sigma', 'p_value', 'x_min']
+    alpha_constrained = [l[0] for l in entries]
+    sigma_constrained = [l[1] for l in entries]
+    pvalue_constrained = [l[2] for l in entries]
+    alpha_unconstrained = [l[3] for l in entries]
+    sigma_unconstrained = [l[4] for l in entries]
+    pvalue_unconstrained = [l[5] for l in entries]
+    xmin_unconstrained = [l[6] for l in entries]
+    distribution = [l[7] for l in entries]
+
+    for e in entries[1:]:
+        #print e[2]
+        distribution = eval(e[7])
+        test = e[2]
+        test = e[5]
+
+        if float(test) == 1.0:# and float(test) != 1.0:
+            #print "p_value is null"
+            distribution = eval(e[7])
+            __drawDistribution(__computeDegreeDistribution(distribution), "indegree", "count", "fit")
+            __drawCCDF(distribution, "indegree", "fit")
+        else:
+            __drawCCDF(distribution, "indegree", "fit", other=True)
+            __drawDistribution(__computeDegreeDistribution(distribution), "indegree", "count", "fit", "red")
+
+        #if float(test) == 1.0:
+        #    print "p-value is one"
+
+    plt.show()
+
+    '''
+
+    avg_alpha_constrained = sum([float(i) for i in alpha_constrained[1:] if i != 'alpha_constrained'])/(len(alpha_constrained)-1)
+    avg_alpha_unconstrained = sum([float(i) for i in alpha_unconstrained[1:] if i != 'alpha'])/(len(alpha_constrained)-1)
+    avg_sigma_unconstrained = sum([float(i) for i in sigma_unconstrained[1:] if i != 'sigma'])/(len(alpha_constrained)-1)
+    avg_sigma_constrained = sum([float(i) for i in sigma_constrained[1:] if i != 'sigma_constrained'])/(len(alpha_constrained)-1)
+    avg_pvalue_constrained = sum([float(i) for i in pvalue_constrained[1:] if i != 'p_value_constrained'])/(len(alpha_constrained)-1)
+    avg_pvalue_unconstrained = sum([float(i) for i in pvalue_unconstrained[1:] if i != 'p_value'])/(len(alpha_constrained)-1)
+
+    if verbose:
+        print 'avg_alpha_constrained:',avg_alpha_constrained
+        print 'avg_sigma_constrained:',avg_sigma_constrained
+        print 'avg_pvalue_constrained:',avg_pvalue_constrained
+        print 'avg_alpha_unconstrained:',avg_alpha_unconstrained
+        print 'avg_sigma_unconstrained:',avg_sigma_unconstrained
+        print 'avg_pvalue_unconstrained:',avg_pvalue_unconstrained
+
+    return {'constrained':{'alpha':avg_alpha_constrained, 'sigma':avg_sigma_constrained, 'p-value':avg_pvalue_constrained},
+            'unconstrained':{'alpha':avg_alpha_unconstrained, 'sigma':avg_sigma_unconstrained, 'p-value':avg_pvalue_unconstrained}}
+    '''
+
+def read_parameters(filename):
+    #Fitting_statistics_2000_1_False_1.0_2500.csv
+    #New_Fitting_statistics_2000_1_True_0.0_0.0_1.0_2500.csv
+    #New_Fitting_statistics_2000_1_True_0.0_0.2_1.0_2500.csv
+
+    parameters = filename.split('.csv')
+    entries = parameters[0].replace("New_","").replace("Fitting_statistics_", "").split("_")
+    #print entries
+    nbnodes = int(entries[0])
+    m = int(entries[1])
+    onboard = entries[2] == "True"
+    nu1 = None
+    nu2 = None
+    if onboard == True:
+        nu1 = float(entries[3])
+        nu2 = float(entries[4])
+        a = float(entries[5])
+        nbruns = int(entries[6])
+    else:
+        a = float(entries[3])
+        nbruns = int(entries[4])
+
+    return {'nbnodes': nbnodes, 'm':m, 'onboard':onboard, 'nu1':nu1, 'nu2':nu2, 'a':a, 'nbruns':nbruns}
+    
+classToFiles = {}
+
+for folder in folders:
+    csv_files = sorted([f for f in os.listdir(folder) if 'csv' in f])
+
+    for f in csv_files:
+        p = read_parameters(f)
+        runclass = "%d_%f_%s_%s_%s_%f_%d"%(p['nbnodes'],p['m'],p['onboard'],p['nu1'],p['nu2'],p['a'],p['nbruns'])
+        if runclass not in classToFiles:
+            classToFiles[runclass]=[]
+        classToFiles[runclass].append(folder+f)
+
+output_folder =  "concatenate/"
+
+for c in sorted(classToFiles):
+    print "processing class ", c, " with ", len(classToFiles[c]), "files"
+    total_lines = []
+    for csv in classToFiles[c]:
+        print "reading file ",csv
+        with open(csv) as f:
+            text = f.read().rstrip().replace('\r','\n')
+            #for l in f:
+            #    print l
+
+            total_lines += text.split('\n')
+
+
+    results = read_csv(total_lines) 
+    #print c, 'unconstrained: ',results['unconstrained']['p-value'], 'constrained: ',results['constrained']['p-value']
+#plt.show()
+    
--- Code/read_res.py
+++ Code/read_res.py
@@ -2,30 +2,46 @@ import os
 #csv = "res.csv"
 #csv = "res_wob.csv"
 
-folders = ["distant-results/", "results/"]
+folders = ["distant-results/", "results/", "../Datasets/FittingData_02_2017/"]
+folders = ["../Datasets/FittingData_02_2017/"]
+folders = ["../Datasets/FittingData/"]
 
 
 def read_csv(lines, verbose=False):
     
     entries = [l.split(';') for l in lines]
-    #print 'the entries are ', entries[0]
+    print 'the entries are ', len(entries[0]), entries[0]
     #print "the rest is ", entries[1]
 
     #['alpha_constrained', 'sigma_constrained', 'p_value_constrained', 'alpha', 'sigma', 'p_value', 'x_min']
-    alpha_constrained = [l[0] for l in entries]
-    sigma_constrained = [l[1] for l in entries]
-    pvalue_constrained = [l[2] for l in entries]
-    alpha_unconstrained = [l[3] for l in entries]
-    sigma_unconstrained = [l[4] for l in entries]
-    pvalue_unconstrained = [l[5] for l in entries]
-    xmin_unconstrained = [l[6] for l in entries]
+    if len(entries[0]) > 5:
+        alpha_constrained = [l[0] for l in entries]
+        sigma_constrained = [l[1] for l in entries]
+        pvalue_constrained = [l[2] for l in entries]
+        alpha_unconstrained = [l[3] for l in entries]
+        sigma_unconstrained = [l[4] for l in entries]
+        pvalue_unconstrained = [l[5] for l in entries]
+        xmin_unconstrained = [l[6] for l in entries]        
 
-    avg_alpha_constrained = sum([float(i) for i in alpha_constrained[1:] if i != 'alpha_constrained'])/(len(alpha_constrained)-1)
-    avg_alpha_unconstrained = sum([float(i) for i in alpha_unconstrained[1:] if i != 'alpha'])/(len(alpha_constrained)-1)
-    avg_sigma_unconstrained = sum([float(i) for i in sigma_unconstrained[1:] if i != 'sigma'])/(len(alpha_constrained)-1)
-    avg_sigma_constrained = sum([float(i) for i in sigma_constrained[1:] if i != 'sigma_constrained'])/(len(alpha_constrained)-1)
-    avg_pvalue_constrained = sum([float(i) for i in pvalue_constrained[1:] if i != 'p_value_constrained'])/(len(alpha_constrained)-1)
-    avg_pvalue_unconstrained = sum([float(i) for i in pvalue_unconstrained[1:] if i != 'p_value'])/(len(alpha_constrained)-1)
+        avg_alpha_constrained = sum([float(i) for i in alpha_constrained[1:] if i != 'alpha_constrained'])/(len(alpha_constrained)-1)
+        avg_alpha_unconstrained = sum([float(i) for i in alpha_unconstrained[1:] if i != 'alpha'])/(len(alpha_constrained)-1)
+        avg_sigma_unconstrained = sum([float(i) for i in sigma_unconstrained[1:] if i != 'sigma'])/(len(alpha_constrained)-1)
+        avg_sigma_constrained = sum([float(i) for i in sigma_constrained[1:] if i != 'sigma_constrained'])/(len(alpha_constrained)-1)
+        avg_pvalue_constrained = sum([float(i) for i in pvalue_constrained[1:] if i != 'p_value_constrained'])/(len(alpha_constrained)-1)
+        avg_pvalue_unconstrained = sum([float(i) for i in pvalue_unconstrained[1:] if i != 'p_value'])/(len(alpha_constrained)-1)
+    else:
+        alpha_constrained = [l[0] for l in entries]
+        pvalue_constrained = [l[1] for l in entries]
+        alpha_unconstrained = [l[2] for l in entries]
+        pvalue_unconstrained = [l[3] for l in entries]
+        xmin_unconstrained = [l[4] for l in entries]        
+
+        avg_alpha_constrained = sum([float(i) for i in alpha_constrained[1:] if i != 'alpha_constrained'])/(len(alpha_constrained)-1)
+        avg_alpha_unconstrained = sum([float(i) for i in alpha_unconstrained[1:] if i != 'alpha'])/(len(alpha_constrained)-1)
+        avg_pvalue_constrained = sum([float(i) for i in pvalue_constrained[1:] if i != 'p_value_constrained'])/(len(alpha_constrained)-1)
+        avg_pvalue_unconstrained = sum([float(i) for i in pvalue_unconstrained[1:] if i != 'p_value'])/(len(alpha_constrained)-1)
+        avg_sigma_unconstrained = None#sum([float(i) for i in sigma_unconstrained[1:] if i != 'sigma'])/(len(alpha_constrained)-1)
+        avg_sigma_constrained = None#sum([float(i) for i in sigma_constrained[1:] if i != 'sigma_constrained'])/(len(alpha_constrained)-1)
 
     if verbose:
         print 'avg_alpha_constrained:',avg_alpha_constrained
@@ -65,9 +81,10 @@ def read_parameters(filename):
 classToFiles = {}
 
 for folder in folders:
-    csv_files = sorted([f for f in os.listdir(folder) if 'csv' in f])
+    csv_files = sorted([f for f in os.listdir(folder) if 'csv' in f and "Fitting" in f and 'all' not in f])
 
     for f in csv_files:
+        print f
         p = read_parameters(f)
         runclass = "%d_%f_%s_%s_%s_%f_%d"%(p['nbnodes'],p['m'],p['onboard'],p['nu1'],p['nu2'],p['a'],p['nbruns'])
         if runclass not in classToFiles:
@@ -76,16 +93,64 @@ for folder in folders:
 
 output_folder =  "concatenate/"
 
+results_nu1 = {}
+results_nu2 = {}
+#2000_1.000000_True_1.0_0.8_1.000000_2500
+
 for c in sorted(classToFiles):
-    #print "processing class ", c, " with ", len(classToFiles[c]), "files"
+    print "processing class ", c, " with ", len(classToFiles[c]), "files"
     total_lines = []
     for csv in classToFiles[c]:
-        #print "reading file ",csv
+        print "reading file ",csv
         with open(csv) as f:
-            text = f.read()
+            text = f.read().rstrip().replace('\r','\n')
+            #for l in f:
+            #    print l
+
             total_lines += text.split('\n')
 
-
+    entries = c.split('_')
+    nu_1 = float(entries[3])
+    nu_2 = float(entries[4])
+    n = int(entries[6])
     results = read_csv(total_lines) 
-    print c, 'unconstrained: ',results['unconstrained']['p-value'], 'constrained: ',results['constrained']['p-value']
+    print n, nu_1, nu_2, 'unconstrained:',results['unconstrained']['p-value'], 'constrained:',results['constrained']['p-value']
+
+    if nu_1 not in results_nu1:
+        results_nu1[nu_1] = {}
+    results_nu1[nu_1][nu_2] = results
+    if nu_2 not in results_nu2:
+        results_nu2[nu_2] = {}
+    results_nu2[nu_2][nu_1] = results
+
+for nu1 in sorted(results_nu1):
+    pvalues = []
+    pvalues_constrained = []
+    for nu2 in sorted(results_nu1[nu1]):
+        res=results_nu1[nu1][nu2]
+        pvalues.append(res['unconstrained']['p-value'])
+    print "nu1 ",nu1, '(', sum(pvalues)/len(pvalues) ,')', pvalues
+for nu1 in sorted(results_nu1):
+    pvalues = []
+    pvalues_constrained = []
+    for nu2 in sorted(results_nu1[nu1]):
+        res=results_nu1[nu1][nu2]
+        pvalues_constrained.append(res['constrained']['p-value'])
+    print "nu1 ",nu1, '(', sum(pvalues_constrained)/len(pvalues_constrained) ,')', pvalues_constrained
+    
+
+for nu2 in sorted(results_nu2):
+    pvalues = []
+    pvalues_constrained = []
+    for nu1 in sorted(results_nu2[nu2]):
+        res=results_nu2[nu2][nu1]
+        pvalues.append(res['unconstrained']['p-value'])
+    print "nu2 ",nu2, '(', sum(pvalues)/len(pvalues) ,')', pvalues
+for nu2 in sorted(results_nu2):
+    pvalues = []
+    pvalues_constrained = []
+    for nu1 in sorted(results_nu2[nu2]):
+        res=results_nu2[nu2][nu1]
+        pvalues_constrained.append(res['constrained']['p-value'])
+    print "nu2 ",nu2, '(', sum(pvalues_constrained)/len(pvalues_constrained) ,')', pvalues_constrained
     
